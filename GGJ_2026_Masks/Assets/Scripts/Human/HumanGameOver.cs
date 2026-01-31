4a24237811f4edf3c9da1f9ded260697927a7998
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-01-31 11:02:35 -0500

    Hacer que se desactive el movimiento del humano cuando gameover

--- GGJ_2026_Masks/Assets/Scripts/Human/HumanGameOver.cs
+++ GGJ_2026_Masks/Assets/Scripts/Human/HumanGameOver.cs
@@ -5,18 +5,29 @@ public class HumanGameOver : MonoBehaviour
     [SerializeField]
     private GameObject spiritGem;
 
-    public float radius = 5f; // Adjust this value as needed
+    [SerializeField]
+    private Movement humanMovement;
 
-    public float angle = 45f; // Adjust this value as needed (in degrees)
+    [SerializeField]
+    private float radius = 5f; // Adjust this value as needed
+
+    [SerializeField]
+    private float angle = 45f; // Adjust this value as needed (in degrees)
 
     public void TimeIsOver()
     {
         Debug.Log("HumanGameOver: Time is over. Triggering game over sequence.");
 
+        // Desactivar el script de movimiento ciclico
+        humanMovement.enabled = false;
+
+        // Encontrar la posición del Spirit Gem y
+        // calcular la nueva posición del humano
+        // pasando los grados a radianes
         Vector3 spiritGemPosition = spiritGem.transform.position;
         float radianAngle = angle * Mathf.Deg2Rad;
 
-
+        // Calcular la nueva posición usando la coordenadas polares
         Vector3 offset = new Vector3(Mathf.Cos(radianAngle) * radius, Mathf.Sin(radianAngle) * radius, 0f);
         transform.position = spiritGemPosition + offset;
 
