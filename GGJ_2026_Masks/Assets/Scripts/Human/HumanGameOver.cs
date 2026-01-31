60204c5c1646619fbd2d87bd1f193fc85d2616b9
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-01-31 10:59:27 -0500

    Hacer que, cuando se acabe el tiempo, el humano spawnee cerca de la gema (En una posición basad en un radio y un angulo)

--- GGJ_2026_Masks/Assets/Scripts/Human/HumanGameOver.cs
+++ GGJ_2026_Masks/Assets/Scripts/Human/HumanGameOver.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class HumanGameOver : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject spiritGem;
+
+    public float radius = 5f; // Adjust this value as needed
+
+    public float angle = 45f; // Adjust this value as needed (in degrees)
+
     public void TimeIsOver()
     {
         Debug.Log("HumanGameOver: Time is over. Triggering game over sequence.");
-        // Aquí puedes agregar la lógica para manejar el final del juego,
-        // como mostrar una pantalla de "Game Over", detener el juego, etc.
+
+        Vector3 spiritGemPosition = spiritGem.transform.position;
+        float radianAngle = angle * Mathf.Deg2Rad;
+
+
+        Vector3 offset = new Vector3(Mathf.Cos(radianAngle) * radius, Mathf.Sin(radianAngle) * radius, 0f);
+        transform.position = spiritGemPosition + offset;
+
     }
 }
